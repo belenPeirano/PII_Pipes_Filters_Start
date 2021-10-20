9ddf51c7e1294e870fb3701f9ec3d58658af14fc
Belen <maria.peirano@correo.ucu.edu.uy>
2021-10-20 17:02:55 -0300

    Ejercicios 1 y 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,28 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            IPipe nulo = new PipeNull();
+
+            IFilter negativo = new FilterNegative();
+
+            IPipe serial = new PipeSerial(negativo, nulo);
+
+            IFilter grises = new FilterGreyscale();
+
+            IPipe serial2 = new PipeSerial(grises, serial);
+
+            IPicture fotoNegativa = serial.Send(picture);
+            provider.SavePicture(fotoNegativa, @"Cerveza-Negativa.jpg");
+
+            IPicture fotoGris = serial2.Send(picture);
+            provider.SavePicture(fotoGris, @"Foto-Gris.jpg");
+
+            
+
+
         }
     }
 }
